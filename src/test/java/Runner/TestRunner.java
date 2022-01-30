43a0a063f7fcd16e25d59ea39da0e7216f7d00b4
Upendra <upenndra137@gmail.com>
2022-01-30 15:31:32 +0530

    updated crome iptions

---
+++ src/test/java/Runner/TestRunner.java
@@ -0,0 +1,25 @@
+package Runner;
+
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "resources/Feature"
+		,glue={"stepDefinition"}
+		)
+
+public class TestRunner {
+
+}
+
+
+//def cucumberOptions = ['-p', 'pretty',
+//                       '--add-plugin', 'de.monochromata.cucumber.report.PrettyReports:TestResults/Reports/cucumber-html',
+//                       '-p', 'json:TestResults/Reports/cucumber-report.json',
+//                       '-g', 'gradle.cucumber',
+//                       '-g', 'core.setup',
+//                       '-g', 'core.test.steps',
+//                       'src/test/resources',]
